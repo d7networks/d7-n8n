09ec51f94f025010e362118bb7b205d0947ddd36
kichukic <kichukic@gmail.com>
2025-01-17 16:45:00 +0530

    file structure changed (from PR review)

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.D7Api = exports.D7Messaging = void 0;
-const D7Messaging_node_1 = require("./nodes/D7Messaging.node");
+const D7Messaging_node_1 = require("./nodes/D7Messaging/D7Messaging.node");
 Object.defineProperty(exports, "D7Messaging", { enumerable: true, get: function () { return D7Messaging_node_1.D7Messaging; } });
 const D7Api_credentials_1 = require("./credentials/D7Api.credentials");
 Object.defineProperty(exports, "D7Api", { enumerable: true, get: function () { return D7Api_credentials_1.D7Api; } });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {D7Messaging} from './nodes/D7Messaging.node';
+import {D7Messaging} from './nodes/D7Messaging/D7Messaging.node';
 import {D7Api} from     './credentials/D7Api.credentials';
 
 export {D7Messaging,D7Api};  // Export your custom node so n8n can recognize it
